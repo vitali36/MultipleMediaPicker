d1ff11ae459b84837b132e0f2b9a0cdbabe283cc
Vitaliy <vitali.bulavkin@gmail.com>
2020-09-23 09:46:39 +0300

    Initial project setup

---
+++ MultipleMediaPicker/MultipleMediaPicker.h
@@ -0,0 +1,18 @@
+//
+//  MultipleMediaPicker.h
+//  MultipleMediaPicker
+//
+//  Created by Vitaliy on 22.09.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MultipleMediaPicker.
+FOUNDATION_EXPORT double MultipleMediaPickerVersionNumber;
+
+//! Project version string for MultipleMediaPicker.
+FOUNDATION_EXPORT const unsigned char MultipleMediaPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MultipleMediaPicker/PublicHeader.h>
+
+
